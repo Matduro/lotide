ac5590a4d9b64ef3a79d22dbb214ce65b1e63164
Mathieu Rousseau <mathieurousseau123@gmail.com>
2021-03-03 01:41:18 +0000

    updated with a eqArrays code

---
+++ eqArrays.js
@@ -0,0 +1,42 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  // const stopEmoji = U+1F6D1;
+  // const checkEmoji = U+2705;
+
+  //const trueFalse = console.assert(actual, expected);
+  //console.log(trueFalse);
+  if (actual === expected) {
+    console.log("✅✅✅" + "Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🆘🆘🆘 " + "Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+const eqArrays = (array1, array2) => {
+  const lenOne = array1.length;
+  const lenTwo = array2.length;
+  if (lenOne !== lenTwo) {
+    return false;
+  }
+  for (let i = 0; i < lenOne; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+/*
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 3); */
---
+++ test.js
@@ -0,0 +1,19 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  // const stopEmoji = U+1F6D1;
+  // const checkEmoji = U+2705;
+
+  //const trueFalse = console.assert(actual, expected);
+  //console.log(trueFalse);
+  if (actual === expected) {
+    console.log(`✅✅✅   Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🆘🆘🆘 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 3);
