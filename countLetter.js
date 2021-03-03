7a099f9bb006161049a8764679d85898487b659c
Mathieu Rousseau <mathieurousseau123@gmail.com>
2021-03-03 21:51:34 +0000

    letter counter

---
+++ countLetter.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🟢🟢🟢" + "Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑" + "Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+const countLetters = (string) => {
+  const count = {};
+  // remove all the space and punctuation characters, then split the letters in an array
+  const arrLetters = string.replace(/[ ,.!?]/g, "").split("");
+  // iterate over the the array of letters, and if the letter key is present in the count object,
+  // then you += the count by 1, else you add the key value pair in the else statement.
+  for (const letter of arrLetters) {
+    if (count[letter]) {
+      count[letter] += 1;
+    } else {
+      count[letter] = 1;
+    }
+  }
+  console.log(count);
+  return count;
+};
+
+countLetters("lighthouse in the house");
